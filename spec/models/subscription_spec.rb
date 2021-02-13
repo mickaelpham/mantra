217a518a8bea4f1be20cf3168d2db459b2c255c0
Mickaël Pham <inbox@mickael.dev>
2021-02-12 20:32:58 -0800

    feat: subscription spec (#1)
    * feat: subscription spec
    
    * fix: change GitHub Actions job name

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,25 @@
+# typed: false
+# frozen_string_literal: true
+
+RSpec.describe Subscription do
+  let(:a_product)      { Product.new(name: 'My Wonderful Product') }
+  let(:a_sku)          { SKU.new(product: a_product, quantity: 2) }
+  let(:another_sku)    { SKU.new(product: a_product, quantity: 5) }
+  let(:a_subscription) { Subscription.new(skus: [a_sku]) }
+
+  it 'has a start time' do
+    expect(a_subscription.starts_at).to be_a(Time)
+  end
+
+  it 'has an expiration time, greater than the start time' do
+    expect(a_subscription.ends_at).to be > a_subscription.starts_at
+  end
+
+  it 'has an array of SKUs' do
+    expect(a_subscription.skus).to all(be_a(SKU))
+  end
+
+  it 'accepts additional SKUs' do
+    expect { a_subscription.skus << another_sku }.to change { a_subscription.skus.size }.from(1).to(2)
+  end
+end
